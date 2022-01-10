86bd4aad2fe161ce00b368d06f913ac521d09e7c
liuxudong <570959422@qq.com>
2022-01-10 18:01:07 +0800

    Initial commit

---
+++ src/main/java/Member.java
@@ -0,0 +1,40 @@
+/*
+* 购物会员
+* */
+
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Date;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Member {
+    @ExcelProperty("ID")
+    @ColumnWidth(10)
+    private Long id;
+    @ExcelProperty("用户名")
+    @ColumnWidth(20)
+    private String username;
+    @ExcelIgnore
+    private String password;
+    @ExcelProperty("昵称")
+    @ColumnWidth(20)
+    private String nickname;
+    @ExcelProperty("出生日期")
+    @ColumnWidth(20)
+    @DateTimeFormat("yyyy-MM-dd")
+    private Date birthday;
+    @ExcelProperty("手机号")
+    @ColumnWidth(20)
+    private String phone;
+    @ExcelIgnore
+    private String icon;
+    @ExcelProperty(value = "性别",converter = GenderConverter.class)
+    @ColumnWidth(20)
+    private Integer gender;
+}
